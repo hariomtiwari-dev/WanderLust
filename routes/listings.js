600317a4d75bd1d564277b74ee98e4966e8f3e24
hariotiwari003 <hariompstiwari@gmail.com>
2026-08-17 18:16:07 +0530

    flash message

--- routes/listings.js
+++ routes/listings.js
@@ -7,7 +7,6 @@ const Listing = require("../models/listing.js");
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
-
     if (error) {
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
