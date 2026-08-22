11c6f67947ef77404e796afbf71ad51231eedc33
hariotiwari003 <hariompstiwari@gmail.com>
2026-08-22 20:40:23 +0530

    controller

--- routes/listings.js
+++ routes/listings.js
@@ -1,69 +1,52 @@
+
+
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
-const { isLoggedIn, isOwner, validateListing} = require("../middleware.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer  = require('multer')
+const { storage } = require("../cloudconfig.js");
+const upload = multer({ storage });
 
 
-
-// Index Route
-router.get("/", wrapAsync(listingController.index));
+router
+    .route("/")
+    .get(wrapAsync(listingController.index))
+    .post(
+        isLoggedIn,
+        upload.single("listing[image]"),
+        validateListing,
+        wrapAsync(listingController.createListing)
+    )
+   
 
 //New Route
-router.get("/new", isLoggedIn,listingController.renderNewForm);
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
+router
+    .route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(
+        isLoggedIn,
+        isOwner,
+        validateListing,
+        upload.single("listing[image]"),
+        wrapAsync(listingController.updateListing)
+    )
+    .delete(
+        isLoggedIn,
+        isOwner,
+        wrapAsync(listingController.destroyListing)
+    );
 
-
-// Show Route
-router.get(
-    "/:id", wrapAsync(listingController.showListing)
-   );
-
-
-// Create Route
-router.post(
-    "/",
-    isLoggedIn,
-    validateListing,
-    wrapAsync(listingController.createListing)
-);
-
-// Edit Route
+//Edit Route
 router.get(
     "/:id/edit",
     isLoggedIn,
-     isOwner ,
+    isOwner,
     wrapAsync(listingController.renderEditForm)
 );
-
-//Update Route
-router.put(
-    "/:id",
-    isLoggedIn,
-    isOwner ,
-    validateListing,
-    wrapAsync(listingController.updateListing)
-);
-
-// Delete Route
-router.delete("/:id",
-    isLoggedIn,
-     isOwner ,
-    wrapAsync(listingController.destroyListing));
-
-// Show Route
-router.get("/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-
-    const listing = await Listing.findById(id).populate("reviews");
-
-    if (!listing) {
-        req.flash("error", "Listing you requested for does not exist!");
-        return res.redirect("/listings");
-    }
-
-    res.render("listings/show.ejs", { listing });
-}));
 
 module.exports = router;
